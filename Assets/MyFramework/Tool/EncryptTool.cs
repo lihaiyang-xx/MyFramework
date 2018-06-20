3d94fd84388b7d2f46218d19d5b31f6edf5d7aed
lihaiyang <762923309@qq.com>
2018-06-20 17:34:55 +0800

    添加对称加密算法

--- Assets/MyFramework/Tool/EncryptTool.cs
+++ Assets/MyFramework/Tool/EncryptTool.cs
@@ -51,4 +51,40 @@ public static class EncryptTool
             throw new Exception("md5file() fail, error:" + ex.Message);
         }
     }
+
+    /// <summary>
+    /// 对称加密与解密
+    /// </summary>
+    /// <param name="encryptStr"></param>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public static string Encrypt(string encryptStr,string key = "aescodtuy@#p^sfd")
+    {
+        byte[] keyArray = Encoding.UTF8.GetBytes(key);
+        byte[] toEncryptArray = Encoding.UTF8.GetBytes(encryptStr);
+        RijndaelManaged rDel = new RijndaelManaged
+        {
+            Key = keyArray,
+            Mode = CipherMode.ECB,
+            Padding = PaddingMode.PKCS7
+        };
+        ICryptoTransform cTransform = rDel.CreateEncryptor();
+        byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+        return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+    }
+
+    public static string Decrypt(string decryptStr, string key = "aescodtuy@#p^sfd")
+    {
+        byte[] keyArray = Encoding.UTF8.GetBytes(key);
+        byte[] toEncryptArray = Convert.FromBase64String(decryptStr);
+        RijndaelManaged rDel = new RijndaelManaged
+        {
+            Key = keyArray,
+            Mode = CipherMode.ECB,
+            Padding = PaddingMode.PKCS7
+        };
+        ICryptoTransform cTransform = rDel.CreateDecryptor();
+        byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+        return Encoding.UTF8.GetString(resultArray);
+    }
 }
